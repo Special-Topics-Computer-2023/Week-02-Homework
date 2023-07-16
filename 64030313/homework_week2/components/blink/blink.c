b4f6cfc0b4e99aedc65321b6eafbaf854a1c459a
NamaoySudarat <64030313@kmitl.ac.th>
2023-07-16 21:37:43 +0700

    edit code project01

--- 64030313/homework_week2/components/blink/blink.c
+++ 64030313/homework_week2/components/blink/blink.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-#include "blink.h"
+#include <stdbool.h>
+#include <unistd.h>
+#include "driver/gpio.h"
 
-void func(void)
+void blink(void)
 {
+    gpio_reset_pin(22);
+    gpio_set_direction(22, GPIO_MODE_OUTPUT);
 
+    while (true)
+    {
+        gpio_set_level(22, 1);
+        usleep(500000);
+        gpio_set_level(22, 0);
+        usleep(500000);
+    }
 }
